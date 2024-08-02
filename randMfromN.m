3505426463a4bc8f6a0b843a4a3d22a12b247b70
theronHalsey <34664651+theronhalsey@users.noreply.github.com>
2024-08-02 17:51:15 -0400

    algo s running

---
+++ randMfromN.m
@@ -0,0 +1,19 @@
+function rand_selections = randMfromN(options,N_options,M_toChoose)
+% uses Knuth's Algorithm S to select M itemsvfrom a list of N options with
+% equal probability
+n_chosen = 0;
+r_toChoose = M_toChoose;
+r_options = N_options;
+rand_selections = zeros(1,M_toChoose);
+for i=1:N_options   
+    rand_int = randi(r_options);
+    r_options = r_options - 1;
+    if rand_int <= r_toChoose
+        n_chosen = n_chosen + 1;
+        r_toChoose = r_toChoose - 1;
+        rand_selections(n_chosen) = options(i);
+    end
+    if ~r_toChoose
+        break;
+    end
+end
