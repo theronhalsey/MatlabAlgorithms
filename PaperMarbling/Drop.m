c7838a7ca286dc9bd50a27daef69231e0c33a86c
theronHalsey <34664651+theronhalsey@users.noreply.github.com>
2024-08-27 16:41:26 -0400

    rendering bug to work out...

--- PaperMarbling/Drop.m
+++ PaperMarbling/Drop.m
@@ -5,33 +5,39 @@ classdef Drop < handle
         x {mustBeNumeric}
         y {mustBeNumeric}
         r {mustBeNonnegative}
-        d
-        n_vertices
-        theta
-        angles
+        c
         vertices
     end
 
     methods
-        function obj = Drop(x_in,y_in,r_in)
+        function obj = Drop(x_in,y_in,r_in,c_in)
             %Drop Constructor
             %   assign the x and y values of the location and radius r of a
             %   drop
             obj.x = x_in;
             obj.y = y_in;
             obj.r = r_in;
-            obj.d = 2*r_in;
-            obj.n_vertices = 100*r_in;
-            obj.theta = tau/obj.n_vertices; 
-            obj.angles = (0:obj.n_vertices-1)*obj.theta;
-            obj.vertices = [arrayfun(@(t) cos(t), obj.angles); arrayfun(@(t) sin(t), obj.angles)]*obj.r + [obj.x;obj.y];
+            obj.c = c_in;
+            
+            n_vertices = 100*r_in;
+            theta = tau/n_vertices; 
+            angles = (0:n_vertices-1)*theta;
+            
+            obj.vertices = [arrayfun(@(t) cos(t), angles); arrayfun(@(t) sin(t), angles)]*obj.r + [obj.x;obj.y];
         end
 
-        function vertices = place(obj)
+        function place(obj)
             %place a drop of ink into the plot
-            vertices = obj.vertices;
-            fill(obj.vertices(1,:),obj.vertices(2,:),'r',LineStyle='none')
+            fill(obj.vertices(1,:),obj.vertices(2,:),obj.c,EdgeColor='white')
             drawnow
+        end
+
+        function marble(obj,otherDrop)
+            %marbles existing drop when another drop is added
+            otherCenter = [otherDrop.x; otherDrop.y];
+            dist = obj.vertices-otherCenter;
+            mags = arrayfun(@(x,y) sqrt(x^2 + y^2), dist(1,:), dist(2,:));
+            obj.vertices = otherCenter + (dist) .* sqrt(1 + (otherDrop.r^2) ./ (mags.^2));
         end
     end
 end
--- PaperMarbling/PaperMarbling.m
+++ PaperMarbling/PaperMarbling.m
@@ -7,18 +7,29 @@ axis([0,xmax,0,ymax])
 hold on
 
 n_drops = 3;
+colors = ['r','g','b'];
 
 [c_x,c_y] = ginput(1);
 [e_x,e_y] = ginput(1);
 r = sqrt((e_x - c_x)^2 + (e_y - c_y)^2);
-Drops = Drop(c_x,c_y,r);
+Drops = Drop(c_x,c_y,r,colors(1));
 Drops.place;
 
 for i=2:n_drops
+    % get new drop
     [c_x,c_y] = ginput(1);
     [e_x,e_y] = ginput(1);
     r = sqrt((e_x - c_x)^2 + (e_y - c_y)^2);
-    nextDrop = Drop(c_x,c_y,r);
-    nextDrop.place;
-    Drops(1,i) = nextDrop;
+    newDrop = Drop(c_x,c_y,r,colors(i));
+    
+    % marble old drops from influence of new
+    l = length(Drops);
+    for j=1:l
+        Drops(j).marble(newDrop)
+        Drops(j).place
+    end
+    newDrop.place;
+
+    % add new drop to Drops array
+    Drops(1,i) = newDrop;
 end
